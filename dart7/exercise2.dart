df6c8acc44f9825a4d8225975ae3bb47c974ca11
ronaksarvaiya19 <ronaksarvaiya4397@gmail.com>
2023-09-15 20:00:55 +0530

    new

--- dart7/exercise2.dart
+++ dart7/exercise2.dart
@@ -1,7 +1,9 @@
+// Write a programe to findout how many millineum year does the list have
+// [1000,2360,5220,9800,4100,5000,6000,4500]
+
 void main() {
   List<int> years = [1000, 2360, 5220, 9800, 4100, 5000, 6000, 4500];
 
-  int millenneumYears = years.where((years) => years % 1000 == 0).length ;
+  int millenneumYears = years.where((years) => years % 1000 == 0).length;
   print(millenneumYears);
-
 }
