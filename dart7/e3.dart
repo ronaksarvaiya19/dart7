d1aaf4a550ef5a003a157f2544b2fa6afa3a2294
ronaksarvaiya19 <ronaksarvaiya4397@gmail.com>
2023-10-23 12:37:45 +0530

    new

--- dart7/e3.dart
+++ dart7/e3.dart
@@ -7,13 +7,22 @@ void main() {
   print("Enter amy one alphabet");
   alphabet = stdin.readLineSync().toString();
 
-  if (alphabet == 'a' || alphabet == 'e' || alphabet == 'i' || alphabet == 'o' || alphabet == 'u' || alphabet == 'A' || alphabet == 'E' || alphabet == 'I' || alphabet == 'O' || alphabet == 'U')
-   {
-    print("the given alphabet is vowel");
-  } 
-
-  else
-   {
-    print("the given alphabet is consonent");
+  if (alphabet.length == 1) {
+    if (alphabet == 'a' ||
+        alphabet == 'e' ||
+        alphabet == 'i' ||
+        alphabet == 'o' ||
+        alphabet == 'u' ||
+        alphabet == 'A' ||
+        alphabet == 'E' ||
+        alphabet == 'I' ||
+        alphabet == 'O' ||
+        alphabet == 'U') {
+      print("the given alphabet is vowel");
+    } else {
+      print("the given alphabet is consonent");
+    }
+  } else {
+    print("Enter only one alphabet");
   }
 }
